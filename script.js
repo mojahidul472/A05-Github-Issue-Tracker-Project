79ef2522f369856ccc1547cf356102ff78aaa8d1
Mojahid <mdmojahidul472@gmail.com>
2026-03-19 19:34:37 +0600

    onclick btn code

--- script.js
+++ script.js
@@ -34,16 +34,16 @@ function render(data) {
         if (p === 'high') {
             pClass = 'text-[#C53030] bg-[#FFF5F5] border-[#FEB2B2]';
             leftIconColor = "text-[#C53030]";
-            leftIcon = `<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" stroke-width="2.5"></path></svg>`;
+            leftIcon = `<img src="assets/Open-Status.png" class="w-5 h-5 object-contain" alt="High">`;
         } else if (p === 'medium') {
             pClass = 'text-[#B7791F] bg-[#FFFFF0] border-[#F6E05E]';
             leftIconColor = "text-[#38A169]";
-            leftIcon = `<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" stroke-width="2.5"></path></svg>`;
+            leftIcon = `<img src="assets/Open-Status.png" class="w-5 h-5 object-contain" alt="High">`;
         } else {
             // Low এর জন্য টিক মার্ক
             pClass = 'text-[#2B6CB0] bg-[#EBF8FF] border-[#90CDF4]';
             leftIconColor = "text-[#2B6CB0]";
-            leftIcon = `<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M5 13l4 4L19 7" stroke-width="3"></path></svg>`;
+            leftIcon = `<img src="assets/Closed- Status .png" class="w-5 h-5 object-contain" alt="High">`;
         }
 
         return `
